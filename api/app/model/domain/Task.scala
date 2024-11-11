e85fa823b01382903e5ed1b22f68b0d12b6698ec
ReidoBoss <bryansagarino222@gmail.com>
2024-11-11 09:07:07 +0800

    Added json writes for task case class

--- api/app/model/domain/Task.scala
+++ api/app/model/domain/Task.scala
@@ -1,6 +1,6 @@
 package domain.task
 import java.util.{ UUID , Date}
-
+import play.api.libs.json._
 
 case class Habit(
   name: String,
@@ -8,4 +8,38 @@ case class Habit(
   isCompleted:Boolean,
   date: Date,
   id: UUID = UUID.randomUUID()
-);
+) {
+
+  def toJson = Json.obj(
+    "id" -> id,
+    "name" -> name,
+    "goal" -> goal,
+    "isCompleted" -> isCompleted,
+    "date" -> date,
+  )
+}
+
+object Habit {
+
+  def apply (
+    name: String,
+    goal: String,
+    isCompleted:Boolean,
+    date: Date
+  ) = {
+    new Habit(name,goal,isCompleted,date)
+  }
+
+  def unapply( habit: Habit ) = {
+    Some((
+      habit.name,
+      habit.goal,
+      habit.isCompleted,
+      habit.date
+    ))
+  }
+
+  given Writes[Habit] = Json.writes[Habit]
+
+}
+
